6d863f89efb7ff91cbfa59cee8fac8a2cf2766cf
Xinyi Chen <xinyichen03@163.com>
2025-03-01 12:23:00 +0800

    add app

--- headless.py
+++ headless.py
@@ -1,18 +1,61 @@
+import os
 import requests
+import anthropic
 from time import sleep
+from flask import Flask, render_template, request, Response, stream_with_context
 from typing import List, Dict
 import newspaper  # alternative to beautifulsoup
-import anthropic
-import os
 
 BRAVE_API_KEY = ''
 ANSWER_PROMPT = ''
 
+app = Flask(__name__)
+
 client = anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
 
-def ask(query: str):
-    # Given a user query, fetch search results and create a response
-    pass
+
+@app.route("/")
+def index():
+    # Home page
+    return render_template("index.html")
+
+def stream_response(query: str):
+    unique_urls = set()
+    full_results = []
+
+    yield (f"Searching for {query}...\n")
+    yield ("\n")
+
+    search_results = get_search_results(query)
+    for search_result in search_results:
+        if search_result["url"] not in unique_urls:
+            yield (f"Reading {search_result['title']}\n")
+            unique_urls.add(search_result["url"])
+            search_result["content"] = get_url_content(search_result["url"])
+            search_result["id"] = len(full_results) + 1
+            full_results.append(search_result)
+
+    yield from generate_response(query, full_results)
+
+@app.route("/ask", methods=["POST"])
+def ask():
+    # Ask page (takes a query and returns a response)
+    query = request.json["query"]
+    unique_urls = set()
+    full_results = []
+
+    search_results = get_search_results(query)
+    for search_result in search_results:
+        if search_result["url"] not in unique_urls:
+            print(f"Reading {search_result['title']}")
+            unique_urls.add(search_result["url"])
+            search_result["content"] = get_url_content(search_result["url"])
+            search_result["id"] = len(full_results) + 1
+            full_results.append(search_result)
+
+    return Response(
+        stream_with_context(stream_response(query)), content_type="text/plain"
+    )
 
 def get_search_results(search_query: str, limit: int = 3):
     # Search using Brave Search API
@@ -62,9 +105,9 @@ def generate_response(query: str, results: List[Dict]) -> str:
         stop_sequences=["</answer>"],
     ) as stream:
         for text in stream.text_stream:
-            print(text, end="", flush=True)
+            yield (text)
 
-    print("\n\n")
-    print("Sources:")
+    yield ("\n")
+    yield ("Sources:\n")
     for result in results:
-        print(f"{result['id']}. {result['title']} ({result['url']})")
+        yield (f"{result['id']}. {result['title']} ({result['url']})\n")
